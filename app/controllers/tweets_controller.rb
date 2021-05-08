edbe52b4eed463b1d7913bd977e28fd5428194be
joshuaretallick <josh@old-empire.co.uk>
2021-05-08 19:46:48 +0100

    Created association between User and Tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,7 +24,7 @@ class TweetsController < ApplicationController
   # POST /tweets or /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
-
+    @tweet.user = current_user
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_174440) do
+ActiveRecord::Schema.define(version: 2021_05_08_183428) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_08_174440) do
     t.string "picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
